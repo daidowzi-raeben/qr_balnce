4b909c6c1f42d39c72fbc473856847c4730a9a57
장영욱 <raeben@naver.com>
2025-09-12 14:45:03 +0900

    fix

--- api/data/dbconfig.php
+++ api/data/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit;
-define('G5_MYSQL_HOST', '119.64.74.78:5998');
+define('G5_MYSQL_HOST', 'localhost');
 define('G5_MYSQL_USER', 'root');
 define('G5_MYSQL_PASSWORD', 'Wkdduddnr1!');
 define('G5_MYSQL_DB', 'qr_balance');
